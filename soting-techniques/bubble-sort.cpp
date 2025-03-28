ca982e79d3677391adc1e6bbcdfd83bd4becf2a0
ravinder <ravibeniwal931@gmail.com>
2025-03-29 00:15:50 +0530

    bubble sort

--- soting-techniques/bubble-sort.cpp
+++ soting-techniques/bubble-sort.cpp
@@ -1,7 +1,18 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-    
-    return 0;
-}
+class Solution {
+    public:
+      // Function to sort the array using bubble sort algorithm.
+      void bubbleSort(vector<int>& arr) {
+          int n = arr.size();
+          for(int i=n; i>0;i--){
+              for(int j = 0 ; j< i-1;j++){
+                  if(arr[j]>arr[j+1]){
+                      int temp = arr[j];
+                      arr[j] = arr[j+1];
+                      arr[j+1] = temp;
+                  }
+                  
+              }
+              
+          }
+      }
+  };
